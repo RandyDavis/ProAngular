431089a3f39646e5bbb966311a788d6fc81ef4f3
Randy Davis <randy_davis@outlook.com>
2015-06-14 00:47:47 -0500

    finish navigation and sorting for which categories are selected via the navigation

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -3,9 +3,9 @@ angular.module("sportsStore")
     $scope.data = {
       products: [
         { name: "Product #1", description: "A product", category: "Category #1", price: 100 },
-        { name: "Product #2", description: "A product", category: "Category #2", price: 110 },
-        { name: "Product #3", description: "A product", category: "Category #3", price: 210 },
-        { name: "Product #4", description: "A product", category: "Category #4", price: 202 }
+        { name: "Product #2", description: "A product", category: "Category #1", price: 110 },
+        { name: "Product #3", description: "A product", category: "Category #2", price: 210 },
+        { name: "Product #4", description: "A product", category: "Category #3", price: 202 }
       ]
     };
   }]);
