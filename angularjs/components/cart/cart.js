eb03d46617aab09b7a0c5c9a6aba2920351e09c6
Randy Davis <randy_davis@outlook.com>
2015-06-16 00:30:31 -0500

    create cart factory

---
+++ angularjs/components/cart/cart.js
@@ -0,0 +1,35 @@
+angular.module("cart", [])
+  .factory("cart", function() {
+    var cartData = [];
+
+    return {
+      addProduct: function(id, name, price) {
+        var addedToExistingItem = false;
+        for(var i = 0; i < cartData.length; i++) {
+          if(cartData[i].objectId == id) {
+            cartData[i].count++;
+            addedToExistingItem = true;
+            break;
+          }
+        }
+        if(!addedToExistingItem) {
+          cartData.push({
+            count: 1, objectId: id, price: price, name: name
+          });
+        }
+      },
+
+      removeProduct: function(id) {
+        for(var i = 0; i < cartData.length; i++) {
+          if(cartData[i].objectId == id) {
+            cartData.splice(i, 1);
+            break;
+          }
+        }
+      },
+
+      getProducts: function() {
+        return cartData;
+      }
+    }
+  });
