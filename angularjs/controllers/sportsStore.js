8febd5ad4f6db11c71da4211f3919a9c36878de4
Randy Davis <randy_davis@outlook.com>
2015-06-24 23:41:52 -0500

    Update and add new files to complete the checkout process

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,6 +1,7 @@
 angular.module("sportsStore")
   // Using Real Product Data from Parse
   .constant('dataUrl', "https://api.parse.com/1/classes/Products")
+  .constant("orderUrl", "https://api.parse.com/1/classes/Orders")
   .run(function($http) {
     $http.defaults.headers.common["X-Parse-Application-Id"]
       = "Ha2eXBEXMvjW06am7mYHP1hf3yLWCDfT6ZaHp00E";
@@ -8,7 +9,7 @@ angular.module("sportsStore")
       = "9mZ4uPmBt5gyItlqthpBPafD5bHaMzHIGZpDne3p";
   })
 
-  .controller("sportsStoreCtrl", ["$scope", "$http", "dataUrl", function($scope, $http, dataUrl) {
+  .controller("sportsStoreCtrl", ["$scope", "$http", "$location", "dataUrl", "orderUrl", "cart" , function($scope, $http, $location, dataUrl, orderUrl, cart) {
     $scope.data = {};
 
     $http.get(dataUrl)
@@ -18,6 +19,21 @@ angular.module("sportsStore")
       .error(function(response) {
         $scope.data.error = response.error || response;
       });
+
+    $scope.sendOrder = function(shippingDetails) {
+      var order = angular.copy(shippingDetails);
+      order.products = cart.getProducts();
+      $http.post(orderUrl, order)
+        .success(function(data) {
+          $scope.data.orderId = data.objectId;
+          cart.getProducts().length = 0;
+        })
+        .error(function(error) {
+          $scope.data.orderError = error;
+        }).finally(function() {
+          $location.path("/complete");
+        });
+    };
   }]);
 
 
