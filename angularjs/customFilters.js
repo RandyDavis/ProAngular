0b3fb352e1c8c41da084ce1a192ff850214f6182
Randy Davis <randy_davis@outlook.com>
2015-10-19 22:52:02 -0500

    add 'skip' filter example

--- angularjs/customFilters.js
+++ angularjs/customFilters.js
@@ -14,3 +14,18 @@ angular.module("exampleApp")
             }
         }
     })
+
+    // add "skip" filter for listing14-21
+    .filter("skip", function () {
+        return function (data, count) {
+            if (angular.isArray(data) && angular.isNumber(count)) {
+                if (count > data.length || count < 1) {
+                    return data;
+                } else {
+                    return data.slice(count);
+                }
+            } else {
+                return data;
+            }
+        }
+    });
